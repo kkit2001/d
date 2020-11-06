77f7b37fc10308567586d7d95165269a913dd17c
kkit2001 <73679898+kkit2001@users.noreply.github.com>
2020-11-06 09:55:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -30,14 +30,5 @@
         </div> <!--Close comtainer-->
         <title>Hello World</title>
     </head>
-    <body>
-        <h1>กิตติพจน์ กิจสุภาพศิริกุล</h1>
-        <p>กิต</p>
-        <img src="tokyo.jpg">
-        <img src="tokyo2.jpg">
-        <br>
-        <a href="https://www.japan-guide.com/e/e2164.html">This is a Link For Web</a>
-        <p>พรุ่งนี้ส่งงานpsit ปลตลกร้ายhahahahaha</p>
-    </body>
 </html>
 
