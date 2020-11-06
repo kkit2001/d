3b6b4b90cad276f385be657b07b87152b89e307c
kkit2001 <73679898+kkit2001@users.noreply.github.com>
2020-11-06 09:44:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <html>
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <div class="container">
             <div class="row">
                 <div class="col">
@@ -36,3 +39,4 @@
         <p>พรุ่งนี้ส่งงานpsit ปลตลกร้ายhahahahaha</p>
     </body>
 </html>
+
