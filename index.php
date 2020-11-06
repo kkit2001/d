1e4c058c8c74c4a2360e41dfd94f2344cdb3803c
kkit2001 <73679898+kkit2001@users.noreply.github.com>
2020-11-06 09:36:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
-<html>
-    <head>
-        <title>Hello World</title>
-    </head>
-    <body>
-        <h1>กิตติพจน์ กิจสุภาพศิริกุล</h1>
-        <p>กิต</p>
-        <img src="tokyo.jpg">
-        <img src="tokyo2.jpg">
-        <br>
-        <a href="https://www.japan-guide.com/e/e2164.html">This is a Link For Web</a>
-        <p>พรุ่งนี้ส่งงานpsit ปลตลกร้ายhahahahaha</p>
-
-    </body>
-</html>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <img src="img/banner/banner2.jpg" class="img-fluid">
+        </div>
+    </div> <!-- Close Row 1 -->
+    <div class="row">
+        <div class="col-6">
+            <img src="img/work/work1.jpg" class="img-fluid rounded" />
+            <h1>Rounded</h1>
+            <p>Lorem ipsum dolor sit amet consectetur </p>
+        </div>
+        <div class="col-3">
+            <img src="img/work/work2.jpg" class="img-fluid img-thumbnail" />
+            <h1>Thumbnail</h1>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+        <div class="col-3">
+            <img src="img/work/work3.jpg" class="img-fluid rounded-circle" />
+            <h1>Circle</h1>
+            <p>Lorem ipsum dolor sit amet comsectetur</p>
+        </div>
+    </div> <!-- Close row 2-->
+</div> <!--Close comtainer-->
